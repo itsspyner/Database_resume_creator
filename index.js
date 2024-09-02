a8618cc0879d53fbe80731c8cf1c4192a57d65e4
itsspyner <160397729+itsspyner@users.noreply.github.com>
2024-09-02 21:50:18 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const fs = require('fs');
 var mysql = require('mysql2');
 const bodyParser = require('body-parser');
+const { Writable } = require('stream');
+const { resume } = require('./resumeGenerator.js');
 
 const app = express();
 app.use(bodyParser.json());
@@ -21,42 +22,60 @@ app.post('/jsonData', (req, res) => {
   });
 
   const jsonData = req.body;
-  res.send(jsonData);
 
-  const email = "specturet@gmail.com";
+  const email = 'specturet@gmail.com';
 
-  con.query('insert into resume (email,data) values (?,?)',[email,JSON.stringify(jsonData)], (err, results) => { 
-    if (err) { 
-        console.error('Error inserting data:', err); 
-        return; 
-    } 
 
-    console.log('Data inserted successfully')
-  });
+con.query('insert into resume (email,data) values (?,?)',[email,JSON.stringify(jsonData)], (err, results) => { 
+  if (err) { 
+      console.error('Error inserting data:', err); 
+      return; 
+  } 
 
-  con.query("SELECT data FROM resume WHERE email = 'specturet@gmail.com'", (err, results) => { 
-    if (err) { 
-        console.error('Error fetching data:', err); 
-        return; 
-    } 
-    
-    if (results.length > 0) {
-        const data = results[0].data;
-        
-        try {
-            const parsedData = data;
-            console.log('Query results:', parsedData);
-        } catch (jsonErr) {
-            console.error('Error parsing JSON data:', jsonErr);
-        }
-    } else {
-        console.log('No results found for the given email.');
-    }
+  console.log('Data inserted successfully')
 });
 
+con.query("SELECT data FROM resume WHERE email='specturet@gmail.com'", (err, results) => { 
+  if (err) { 
+      console.error('Error fetching data:', err); 
+      return; 
+  } 
+  
+  if (results.length > 0) {
+      const data = results[0].data;
+      
+      try {
+          const parsedData = data;
+        const doc = resume(parsedData);
+
+        let pdfChunk = [];
+        const bufferStream = new Writable({
+          write(chunk,encodine,next){
+            pdfChunk.push(chunk);
+            next();
+          }
+        });
+
+        doc.pipe(bufferStream);
+
+        bufferStream.on('finish', () => {
+          const pdfBuffer = Buffer.concat(pdfChunk);
+          res.setHeader('Content-Type', 'application/pdf');
+          res.send(pdfBuffer);
+          console.log("Resume created successfully.");
+      });
+
+      } catch (jsonErr) {
+          console.error('Error parsing JSON data:', jsonErr);
+      }
+  } else {
+      console.log('No results found for the given eamil');
+  }
+});
 
 });
 
 app.listen(3000, () => {
   console.log("Listening in port 3000");
-});                 
+});       
+
